ac1a6e228b062a7f3784416c3503e670a1e75377
Aakash Chaturvedi <aakashch@mail.uni-paderborn.de>
2024-12-17 13:53:20 +0100

    Added inventory microservice

---
+++ parent-microservices/inventory-service/src/main/java/com/microservices/inventory/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.microservices.inventory.inventory.repository;
+
+import com.microservices.inventory.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
