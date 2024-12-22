28946b16c62e7d63b3a8794f8f5e052be3d8cd54
Aakash Chaturvedi <aakashch@mail.uni-paderborn.de>
2024-12-22 20:07:46 +0100

    Integrated Mailtrap client for sending email upon kafka event consumption

--- parent-microservices/notification-service/src/main/java/com/mservices/notification/notification_service/service/NotificationService.java
+++ parent-microservices/notification-service/src/main/java/com/mservices/notification/notification_service/service/NotificationService.java
@@ -4,16 +4,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+
 
 @Service
 @Slf4j
 public class NotificationService {
 
+    private final EmailService emailService;
+
+    public NotificationService(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @KafkaListener(topics = "order-events", groupId = "notification-service")
     public void consumeMessage(String message){
-
-        log.info("Received Order Event: {}", message);
-
+        try {
+            System.out.println("Received message: " + message);
+            emailService.sendOrderConfirmationEmail("aakashchaturvedi7@gmail.com", "12");
+        } catch (Exception e) {
+            throw new RuntimeException("Error processing message: " + message, e);
+        }
     }
 
 }
